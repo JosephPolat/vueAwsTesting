fa879d0dce0f7ac3afba12d9029eba501a7fb990
Joseph Polat <joseph@Josephs-MBP.lan>
2022-09-18 09:14:50 -0400

    all

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="This is the new messsage"/>
+    <HelloWorld msg="This is the second new messsage"/>
   </div>
 </template>
 
